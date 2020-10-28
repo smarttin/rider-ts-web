81c1b71c928650003f1aebfa4faddb31d1ebd4e8
smarttin <smarttin77@gmail.com>
2020-10-28 11:12:43 +0100

    basic router setup

--- src/components/App/AppPresenter.tsx
+++ src/components/App/AppPresenter.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+
+import Login from '../../router/Login';
+import PhoneLogin from '../../router/PhoneLogin';
+import VerifyPhone from '../../router/VerifyPhone';
+import SocialLogin from '../../router/SocialLogin';
+import Home from '../../router/Home';
+import Ride from '../../router/Ride';
+import Chat from '../../router/Chat';
+import EditAccount from '../../router/EditAccount';
+import Settings from '../../router/Settings';
+import Places from '../../router/Places';
+import AddPlace from '../../router/AddPlace';
+import FindAddress from '../../router/FindAddress';
 
 interface IProps {
   isLoggedIn: boolean;
 }
 
+const LoggedOutRoutes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Login} />
+    <Route path="/phone-login" component={PhoneLogin} />
+    <Route path="/verify-phone" component={VerifyPhone} />
+    <Route path="/social-login" component={SocialLogin} />
+    <Redirect from="*" to="/" />
+  </Switch>
+);
+
+const LoggedInRoutes: React.FC = () => (
+  <Switch>
+    <Route path="" exact component={Home} />
+    <Route path="/ride/:rideId" exact component={Ride} />
+    <Route path="/chat/:chatId" exact component={Chat} />
+    <Route path="/edit-account" exact component={EditAccount} />
+    <Route path="/settings" exact component={Settings} />
+    <Route path="/places" exact component={Places} />
+    <Route path="/add-place" exact component={AddPlace} />
+    <Route path="/find-address" exact component={FindAddress} />
+    <Redirect from="*" to="/" />
+  </Switch>
+);
+
 const AppPresenter: React.FC<IProps> = ({isLoggedIn}) => (
-  isLoggedIn ? <span>you are in</span> : <span>you are out</span>
+  <Router>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Router>
 );
 
 AppPresenter.propTypes = {
---
+++ src/router/Places/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const R = () => <span>Place</span>
+
+export default R;
